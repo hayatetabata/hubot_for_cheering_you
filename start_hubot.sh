6672d1e7d751cc038b76add7a91a5b0746920e9e
田畑颯 <chinarra.tabata@gmail.com>
2018-03-31 18:22:34 +0900

    Invoke hubot as daemon

--- start_hubot.sh
+++ start_hubot.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN ./node_modules/hubot/bin/hubot --adapter slack
+echo 'Start hubot daemon'
+./node_modules/forever/bin/forever start -c HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN ./node_modules/hubot/bin/hubot --adapter slack
