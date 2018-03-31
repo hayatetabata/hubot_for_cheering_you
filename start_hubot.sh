083d78a112e7ccd2348f821d32f01ac441a22470
田畑颯 <chinarra.tabata@gmail.com>
2018-03-31 19:14:46 +0900

    Fix hubot command

--- start_hubot.sh
+++ start_hubot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
+./node_modules/forever/bin/forever start -c coffee ./node_modules/hubot/bin/hubot --adapter slack
 echo 'Start hubot daemon'
-./node_modules/forever/bin/forever start -c HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN ./node_modules/hubot/bin/hubot --adapter slack
